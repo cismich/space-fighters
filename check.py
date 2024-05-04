7957760833138cb1ebad594b52a9f698e1ae29ca
cismich <45104522+cismich@users.noreply.github.com>
2024-05-04 17:50:06 +0200

    Delete check.py

--- check.py
+++
@@ -1,8 +0,0 @@
-
-#nevim jestli tohle je ten nejlepsi jak zjistit jestli ma nekdo vsechno nainstalovane ale funguje to 
-def init() -> int:
-    try:
-        import curses
-    except:
-        return 1
-    return 0
