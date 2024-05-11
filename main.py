9c84ace975bc5ca39fcbf078de4037b86bca83bd
cismich <45104522+cismich@users.noreply.github.com>
2024-05-11 16:45:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ import os
 
 class scena:
 
-    def __init__(self, root, nazev : str):
+    def __init__(self, root, hra, nazev : str):
+        self.hra = hra
         self.root = root
         self.objects = []
         self.widgets = []
         self.window = tk.Frame(self.root, background = "black")
         self.window.grid(row=1, column=1, sticky="nsew")
         self.isloaded = False
+        self.input = None
 
         self.nazev = nazev
+    
+    def __del__(self):
+       self.unload()
 
     def load(self):
         self.isloaded = True
@@ -24,8 +29,41 @@ class scena:
         self.window.destroy()
 
     def update(self):
+        hra.LoadScene(self.hra, "Menu")
         return
+    def updateInput(self, gameInput : str):
+        match gameInput:
+               case "up":
+                self.input = "up"
+               case "down":
+                self.input = "down"
+               case "left":
+                self.input = "left"
+               case "right":
+                self.input = "right"
+               case "interact":
+                self.input = "interact"
+               case "back":
+                self.input = "back"
+               case "shoot":
+                self.input = "shoot"
 
+
+
+
+class HraScena(scena):
+    def update(self):
+        print("america ya >:P")
+
+class MenuScena(scena):
+    def __init__(self, root, hra, nazev : str):
+       scena.__init__(self, root, hra, nazev)
+       
+       tk.Label(self.window, text= "Space Fighters", background="black", foreground="white", font=("Helvetica", 24)).place(relx=0.365, rely=0.2)
+
+    def update(self):
+       return
+        
 
 
 class hra:
@@ -41,7 +79,6 @@ class hra:
 
        #sceny
        self.currentScene : scena = None
-
 
        #input
        self.root.bind("w", lambda e : self.CaptureInput("up"))
@@ -63,34 +100,23 @@ class hra:
        self.root.after(100, self.update)
    def inputUpdate(self):
        if self.currentInput:
-
-           match self.currentInput:
-               case "up":
-                   print("up")
-               case "down":
-                   print("down")
-               case "left":
-                   print("left")
-               case "right":
-                   print("right")
-               case "interact":
-                   print("interact")
-               case "back":
-                   print("back")
-               case "shoot":
-                   print("shoot")
-
+          self.currentScene.updateInput(self.CaptureInput)
 
        self.currentInput = None
        return
    def GameUpdate(self):
-       #print("game")
+       if self.currentScene:
+          self.currentScene.update()
        return
    def WindowUpdate(self):
        if not self.currentScene:
-           self.currentScene = scena("Menu")
+           self.currentScene = scena(self.root, self, "Main")
        if not self.currentScene.isloaded :
            self.currentScene.load()
+
+   def LoadScene(self, scene : str):
+      if scene == "Menu":
+        self.currentScene = MenuScena(self.root, self, "Menu")
 
 
 
